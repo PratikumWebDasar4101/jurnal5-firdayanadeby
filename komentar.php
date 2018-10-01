7a532bcd6846a92ca9f08f84e552be0815e886ae
firdayanadeby <43123859+firdayanadeby@users.noreply.github.com>
2018-10-01 10:36:39 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,46 @@
 <?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
 
 
 
 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>komentar</title>
+</head>
+<body>
+	<form action=" " method="post">
+<table>
+	<tr>
+		<td>Nama <br>
+		<input type="text" name="nama">
+		</td>
+	</tr>
+
+	<tr>
+		<td>Komentar<br>
+			<textarea name="komentar"></textarea><br><br>
+			<input type="submit" name="submit" value="submit">
+		</td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
+
+<?php
+if (isset($_POST['submit'])) {
+	$nama   = $_POST['nama'];
+	$komentar = $_POST['komentar'];
+	$baru = explode(" ", $komentar);
+	if (count($baru) < 5) {
+		echo "komentar harus lebih dari 5 kata";
+	}else {
+		echo "komentar anda ".$komentar;
+		echo "<br>";
+		echo "jumlah kata :".count($baru);
+	}
+}
 ?>
